2384d1f9a56ca6d97c8978dd22acd75ca267079a
577745752 <577745752@qq.com>
2016-01-22 17:26:23 +0800

    no message

--- CityFriend/Source/CodeClass/Controller/FriendViewController.m
+++ CityFriend/Source/CodeClass/Controller/FriendViewController.m
@@ -16,7 +16,10 @@
 @property(nonatomic,strong)UIButton*groupButton;
 @property(nonatomic,strong)UITableView*friendTableView;
 @property(nonatomic,strong)UITableView*groupTableView;
+//用来记录要添加好友的id
 @property(nonatomic,strong)NSString*addFriendId;
+//用来记录要添加好友的分组
+@property(nonatomic,strong)NSString*addFriendGroup;
 //聊天 相关属性
 @property (nonatomic, strong) AVIMClient *client;
 @end
@@ -32,77 +35,98 @@ static NSString*const cellID=@"cell";
         self.view.backgroundColor=[UIColor greenColor];
         self.right=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(rightClick:)];
         
-
+        
     }
     return self;
 }
 -(void)rightClick:(UIBarButtonItem*)item
 {
-
+    //点击弹窗
     UIAlertController*friendName=[UIAlertController alertControllerWithTitle:@"" message:@"请输入要添加的好友id" preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction*ok=[UIAlertAction actionWithTitle:@"确认添加" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        //获取要添加的用户id
         UITextField*text=(UITextField*)[friendName.view viewWithTag:105];
         self.addFriendId=text.text;
+        //在user表中查询用户是否存在
         AVQuery *query = [AVUser query];
         [query whereKey:@"username" equalTo:text.text];
         [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
             if (error == nil) {
+                //如果返回的用户数组不为空
                 if ([objects count]) {
-                    [self tomSendMessageToJerry];
-//                        AVObject *post = [AVObject objectWithClassName:@"Friends"];
-//                        [post saveInBackground];
+                    //向对方发送添加好友的请求(这里实际上是给对方发送一条消息)
+                    [self SendMessage:[NSString stringWithFormat:@"%@想添加你为好友>_<",[AVUser currentUser].username]];
                 }else{
-                NSLog(@"用户不存在");
+                    NSLog(@"用户不存在");
                 }
             } else {
                 NSLog(@"%@",error);
             }
         }];
-       
+        
     }];
     UIAlertAction*no=[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-       
+        
     }];
     [friendName addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
-                textField.placeholder=@"请输入好友id";
+        textField.placeholder=@"请输入好友id";
         textField.tag=105;
-
-            }];
+    }];
     [friendName addAction:no];
     [friendName addAction:ok];
     [self presentViewController:friendName animated:YES completion:nil];
-
+    
 }
-//发送消息
-- (void)tomSendMessageToJerry {
-    // Tom 创建了一个 client
+// 添加好友的方法(实际上是用了发送消息的方法)
+-(void)SendMessage:(NSString*)message{
+    // 首先 创建了一个 client 来发送消息
     self.client = [[AVIMClient alloc] init];
     //用户登陆状态
     AVUser *currentUser = [AVUser currentUser];
     
-    // Tom 用自己的名字作为 ClientId 打开 client
+    // 用自己的名字作为 ClientId 打开 client
     [self.client openWithClientId:currentUser.username callback:^(BOOL succeeded, NSError *error) {
-        // Tom 建立了与 Jerry 的会话
+        // "我" 建立了与 "对方" 的会话
         [self.client createConversationWithName:@"好友请求" clientIds:@[self.addFriendId] callback:^(AVIMConversation *conversation, NSError *error) {
-            // Tom 发了一条消息给 Jerry
-            [conversation sendMessage:[AVIMTextMessage messageWithText:currentUser.username attributes:nil] callback:^(BOOL succeeded, NSError *error) {
+            // 我 发了一条消息给 对方
+            [conversation sendMessage:[AVIMTextMessage messageWithText:message attributes:nil] callback:^(BOOL succeeded, NSError *error) {
                 if (succeeded) {
                     NSLog(@"发送成功！");
                 }
             }];
         }];
     }];
+
 }
+//- (void)SendMessage{
+//    // 首先 创建了一个 client 来发送消息
+//    self.client = [[AVIMClient alloc] init];
+//    //用户登陆状态
+//    AVUser *currentUser = [AVUser currentUser];
+//    
+//    // 用自己的名字作为 ClientId 打开 client
+//    [self.client openWithClientId:currentUser.username callback:^(BOOL succeeded, NSError *error) {
+//        // "我" 建立了与 "对方" 的会话
+//        [self.client createConversationWithName:@"好友请求" clientIds:@[self.addFriendId] callback:^(AVIMConversation *conversation, NSError *error) {
+//            // 我 发了一条消息给 对方
+//            [conversation sendMessage:[AVIMTextMessage messageWithText:currentUser.username attributes:nil] callback:^(BOOL succeeded, NSError *error) {
+//                if (succeeded) {
+//                    NSLog(@"发送成功！");
+//                }
+//            }];
+//        }];
+//    }];
+//}
 //接收消息
-- (void)jerryReceiveMessageFromTom {
-    // Jerry 创建了一个 client
+- (void)ReceiveMessage{
+    // 用户 创建了一个 client 来接收消息
     self.client = [[AVIMClient alloc] init];
     //用户登陆状态
     AVUser *currentUser = [AVUser currentUser];
     // 设置 client 的 delegate，并实现 delegate 方法
     self.client.delegate = self;
     
-    // Jerry 用自己的名字作为 ClientId 打开了 client
+    // 用户 用自己的名字作为 ClientId 打开了 client
     [self.client openWithClientId:currentUser.username callback:^(BOOL succeeded, NSError *error) {
         // ...
     }];
@@ -113,18 +137,70 @@ static NSString*const cellID=@"cell";
 
 // 接收消息的回调函数
 - (void)conversation:(AVIMConversation *)conversation didReceiveTypedMessage:(AVIMTypedMessage *)message {
-    
-    UIAlertController*get=[UIAlertController alertControllerWithTitle:@"" message:[NSString stringWithFormat:@"%@想添加你为好友",message] preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction*ok=[UIAlertAction actionWithTitle:@"同意" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-             //添加好友代码
-        NSLog(@"可以约了");
-    }];
-    UIAlertAction*no=[UIAlertAction actionWithTitle:@"残忍拒绝" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    //判断接收到的消息是否是好友请求
+    if ([message.text hasSuffix:@"想添加你为好友>_<"]) {//好友请求的处理
+        //截取对方的id
+        NSString*idString=[[message.text componentsSeparatedByString:@"想"] firstObject];
+        UIAlertController*getFriend=[UIAlertController alertControllerWithTitle:@"" message:message.text preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction*ok=[UIAlertAction actionWithTitle:@"同意" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            //获取要添加的用户分组
+            UITextField*text=(UITextField*)[getFriend.view viewWithTag:106];
+            self.addFriendGroup=text.text;
+            //向云端服务器表中添加数据(更新好友表)
+            AVObject *post = [AVObject objectWithClassName:@"Friends"];
+            [post setObject:[AVUser currentUser].username forKey:@"username"];
+            [post setObject:idString forKey:@"friendname"];
+            [post setObject:self.addFriendGroup forKey:@"groupname"];
+            [post save];
+            //刷新ui
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.friendTableView reloadData];
+            });
+            
+            //给请求添加我为好友的用户返回同意添加的结果(让对方同时成功也添加我为好友)
+            [self SendMessage:[NSString stringWithFormat:@"%@已经通过了你的好友请求>_<",[AVUser currentUser].username]];
+        }];
+        UIAlertAction*no=[UIAlertAction actionWithTitle:@"残忍拒绝" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        [getFriend addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            textField.placeholder=@"请输入分组名";
+            textField.tag=106;
+        }];
+
+        [getFriend addAction:no];
+        [getFriend addAction:ok];
+        [self presentViewController:getFriend animated:YES completion:nil];
+    }else if([message.text hasSuffix:@"已经通过了你的好友请求>_<"]){
+        //截取对方的id
+        NSString*idString=[[message.text componentsSeparatedByString:@"已经"] firstObject];
+        UIAlertController*getFriend=[UIAlertController alertControllerWithTitle:@"" message:message.text preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction*ok=[UIAlertAction actionWithTitle:@"搜嘎" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            //获取要添加的用户分组
+            UITextField*text=(UITextField*)[getFriend.view viewWithTag:107];
+            self.addFriendGroup=text.text;
+            //向云端服务器表中添加数据(更新好友表)
+            AVObject *post = [AVObject objectWithClassName:@"Friends"];
+            [post setObject:[AVUser currentUser].username forKey:@"username"];
+            [post setObject:idString forKey:@"friendname"];
+            [post setObject:self.addFriendGroup forKey:@"groupname"];
+            [post save];
+            //刷新ui
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.friendTableView reloadData];
+            });
+        }];
+        [getFriend addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            textField.placeholder=@"请输入分组名";
+            textField.tag=107;
+        }];
+        [getFriend addAction:ok];
+        [self presentViewController:getFriend animated:YES completion:nil];
+    }else{
         
-    }];
-    [get addAction:no];
-    [get addAction:ok];
-    [self presentViewController:get animated:YES completion:nil];
+        //正常聊天
+    }
+   
 }
 
 
@@ -148,28 +224,44 @@ static NSString*const cellID=@"cell";
         [self.view addSubview:self.loginButton];
     }else{
         
-     self.navigationItem.rightBarButtonItem = self.right;   
-    self.headerView=[[UIView alloc]initWithFrame:CGRectMake(0, 64, kWidth, 10*kGap)];
-    [self.view addSubview:self.headerView];
-    self.friendButton=[[UIButton alloc]initWithFrame:CGRectMake(10*kGap, 0, 10*kGap, 10*kGap)];
-    [self.friendButton setImage:[UIImage imageNamed:@"9"] forState:UIControlStateNormal];
-    [self.headerView addSubview:self.friendButton];
-    self.groupButton=[[UIButton alloc]initWithFrame:CGRectMake(30*kGap, 0, 10*kGap, 10*kGap)];
-    [self.groupButton setImage:[UIImage imageNamed:@"10"] forState:UIControlStateNormal];
-    [self.headerView addSubview:self.groupButton];
-
+        
+        
+        self.navigationItem.rightBarButtonItem = self.right;
+        self.headerView=[[UIView alloc]initWithFrame:CGRectMake(0, 64, kWidth, 10*kGap)];
+        [self.view addSubview:self.headerView];
+        self.friendButton=[[UIButton alloc]initWithFrame:CGRectMake(10*kGap, 0, 10*kGap, 10*kGap)];
+        [self.friendButton setImage:[UIImage imageNamed:@"9"] forState:UIControlStateNormal];
+        [self.headerView addSubview:self.friendButton];
+        self.groupButton=[[UIButton alloc]initWithFrame:CGRectMake(30*kGap, 0, 10*kGap, 10*kGap)];
+        [self.groupButton setImage:[UIImage imageNamed:@"10"] forState:UIControlStateNormal];
+        [self.headerView addSubview:self.groupButton];
+        
 #pragma mark------------TableView
+        //TableView的数据
+        AVQuery *query = [AVQuery queryWithClassName:@"Friends"];
+        [query whereKey:@"username" equalTo:[AVUser currentUser].username];
+        [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+            if (!error) {
+                // 检索成功
+                NSLog(@"%@",objects);
+            } else {
+                // 输出错误信息
+                NSLog(@"Error: %@ %@", error, [error userInfo]);
+            }
+        }];
+        
+        
         self.friendTableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 64+10*kGap, kWidth, kHeight-64-10*kGap) style:UITableViewStyleGrouped];
-    self.friendTableView.delegate=self;
-    self.friendTableView.dataSource=self;
-    [self.friendTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
-    self.friendTableView.backgroundColor=[UIColor redColor];
-    [self.view addSubview:self.friendTableView];
-      [self jerryReceiveMessageFromTom];
+        self.friendTableView.delegate=self;
+        self.friendTableView.dataSource=self;
+        [self.friendTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
+        self.friendTableView.backgroundColor=[UIColor redColor];
+        [self.view addSubview:self.friendTableView];
+        [self ReceiveMessage];
     }
     
     
-
+    
     // Do any additional setup after loading the view.
 }
 -(void)loginButtonAction:(UIButton*)button
@@ -191,13 +283,13 @@ static NSString*const cellID=@"cell";
 //设置cell
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
+    
     UITableViewCell*cell=[tableView dequeueReusableCellWithIdentifier:cellID];
     cell.textLabel.text=@"王国维";
-//        CGFloat red=arc4random()%256/255.0;
-//        CGFloat green=arc4random()%256/255.0;
-//        CGFloat blue=arc4random()%256/255.0;
-//        cell.contentView.backgroundColor=[UIColor colorWithRed:red green:green blue:blue alpha:1];
+    //        CGFloat red=arc4random()%256/255.0;
+    //        CGFloat green=arc4random()%256/255.0;
+    //        CGFloat blue=arc4random()%256/255.0;
+    //        cell.contentView.backgroundColor=[UIColor colorWithRed:red green:green blue:blue alpha:1];
     return cell;
 }
 
@@ -266,13 +358,13 @@ static NSString*const cellID=@"cell";
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
