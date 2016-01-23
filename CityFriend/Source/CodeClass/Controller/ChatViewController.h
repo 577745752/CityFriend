ffec953ba62f9ec84c861be860e71b7645bca220
577745752 <577745752@qq.com>
2016-01-23 09:54:48 +0800

    no message

---
+++ CityFriend/Source/CodeClass/Controller/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.h
+//  CityFriend
+//
+//  Created by lanou3g on 16/1/23.
+//  Copyright © 2016年 朱延刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UIViewController
+
+@end
